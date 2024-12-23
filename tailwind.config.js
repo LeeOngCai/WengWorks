22e87dec60f6c30223c13d3baf58124bf2544e38
LeeOngCai <495373094@qq.com>
2024-12-23 23:16:13 +0800

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          800: '#1F2937',
+          900: '#111827',
+        }
+      }
+    },
+  },
+  plugins: [],
+} 
